f10ee5c1bd9a582292e0a17c2927d256b632cc59
Alexander Stratsiaichuk <astratsiaichuk@scnsoft.com>
2016-08-29 13:45:22 +0300

    Fixed negative tests assertions

--- src/test/java/std/NegativeCreateNewFileTest.java
+++ src/test/java/std/NegativeCreateNewFileTest.java
@@ -4,6 +4,7 @@ import junit.framework.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Alex on 28.08.2016.
@@ -14,12 +15,15 @@ public class NegativeCreateNewFileTest extends TestBase  {
     @Test(dataProvider="loadWrongFileNameFromFile")
     public void Negative_ComparingSymbolsTest(String fileName) throws Exception {
         boolean bool = false;
+        Exception exc=null;
         try {
             File f = new File(directoryPathStr + fileName);
             bool = f.createNewFile();
-        } catch (Exception e) {
+        } catch (IOException e) {
+            exc=e;
         }
         finally{
+            Assert.assertTrue(exc!=null && exc.getMessage().equals("The filename, directory name, or volume label syntax is incorrect"));
             Assert.assertFalse(bool);
             Assert.assertFalse(String.format("File %s is created",fileName),isFileExistsInFolder(directoryPathStr,fileName));
         }
@@ -28,17 +32,12 @@ public class NegativeCreateNewFileTest extends TestBase  {
 
     public void Negative_NoNameTest() throws Exception {
 
-        boolean bool = false;
-        try {
-            File f = new File(directoryPathStr);
-            bool = f.createNewFile();
+        boolean bool;
 
-        } catch (Exception e) {
-        }
-        finally{
-            Assert.assertFalse(bool);
-            Assert.assertFalse("File without name was created",isFileExistsInFolder(directoryPathStr,""));
-        }
+        File f = new File(directoryPathStr);
+        bool = f.createNewFile();
+        Assert.assertFalse(bool);
+        Assert.assertFalse("File without name was created",isFileExistsInFolder(directoryPathStr,""));
     }
 
 }
